84efaa0c2445804a81d99335efbfefc3f0b237a7
Krzychooo <Krzysiusiu2>
2018-04-25 08:31:03 +0200

    zamienienie mockow na dane

--- src/app/chart.service.ts
+++ src/app/chart.service.ts
@@ -14,57 +14,57 @@ export class ChartService {
   OdisUrl = 'http://localhost:8080/acct';
 
   getDaysData(): Observable<Array<DayRecord>> {
-    return of(DaysData);
-    // return this.http.get<Array<DayRecord>>(`${this.OdisUrl}/getIpTwoWeeksPackets/${this.IP}`)
-    //   .pipe(
-    //     tap((server: DayRecord[]) => this.logSuccess(`pobrano listę adresów IP`)),
-    //     catchError(this.handleError<Server>(''))
-    //   );
+    // return of(DaysData);
+    return this.http.get<Array<DayRecord>>(`${this.OdisUrl}/getIpTwoWeeksPackets/${this.IP}`)
+      .pipe(
+        tap((server: DayRecord[]) => this.logSuccess(`pobrano listę adresów IP`)),
+        catchError(this.handleError<DayRecord>(''))
+      );
   }
 
   getHoursData(timestamp: string): Observable<Array<DayRecord>> {
-    return of(HoursData);
-    // return this.http.get<Array<DayRecord>>(`${this.OdisUrl}/getIpTwoWeeksPackets/${this.IP}/${timestamp}`)
-    //   .pipe(
-    //     tap((server: DayRecord[]) => this.logSuccess(`pobrano listę adresów IP`)),
-    //     catchError(this.handleError<Server>(''))
-    //   );
+    // return of(HoursData);
+    return this.http.get<Array<DayRecord>>(`${this.OdisUrl}/getIpTwoWeeksPackets/${this.IP}/${timestamp}`)
+      .pipe(
+        tap((server: DayRecord[]) => this.logSuccess(`pobrano listę adresów IP`)),
+        catchError(this.handleError<DayRecord>(''))
+      );
   }
 
   getMinutesData(timestamp: string): Observable<Array<DayRecord>> {
-    return of(MinutesData);
-    // return this.http.get<Array<DayRecord>>(`${this.OdisUrl}/getIpTwoWeeksPackets/${this.IP}/${timestamp}`)
-    //   .pipe(
-    //     tap((server: DayRecord[]) => this.logSuccess(`pobrano listę adresów IP`)),
-    //     catchError(this.handleError<Server>(''))
-    //   );
+    // return of(MinutesData);
+    return this.http.get<Array<DayRecord>>(`${this.OdisUrl}/getIpTwoWeeksPackets/${this.IP}/${timestamp}`)
+      .pipe(
+        tap((server: DayRecord[]) => this.logSuccess(`pobrano listę adresów IP`)),
+        catchError(this.handleError<DayRecord>(''))
+      );
   }
 
   getServersList(): Observable<Array<AcctServer>> {
-    return of(ServersList);
-    // return this.http.get<Array<AcctServer>>(`${this.OdisUrl}/getAllAcct`)
-    //   .pipe(
-    //     tap((server: AcctServer[]) => this.logSuccess(`pobrano listę adresów IP`)),
-    //     catchError(this.handleError<Server>(''))
-    //   );
+    // return of(ServersList);
+    return this.http.get<Array<AcctServer>>(`${this.OdisUrl}/getAllAcct`)
+      .pipe(
+        tap((server: AcctServer[]) => this.logSuccess(`pobrano listę adresów IP`)),
+        catchError(this.handleError<AcctServer>(''))
+      );
   }
 
   getYtTime(IP: string): Observable<Array<YtRecord>> {
-    return of(YtList);
-    // return this.http.get<Array<YtRecord>>(`${this.OdisUrl}/getIpYoutubeAcct/$(IP)`)
-    //   .pipe(
-    //     tap((server: YtRecord[]) => this.logSuccess(`pobrano listę adresów IP`)),
-    //     catchError(this.handleError<Server>(''))
-    //   );
+    // return of(YtList);
+    return this.http.get<Array<YtRecord>>(`${this.OdisUrl}/getIpYoutubeAcct/$(IP)`)
+      .pipe(
+        tap((server: YtRecord[]) => this.logSuccess(`pobrano listę adresów IP`)),
+        catchError(this.handleError<YtRecord>(''))
+      );
   }
 
   getBytes(IP: string): Observable<Array<ServerBytes>> {
-    return of(BytesList);
-    // return this.http.get<Array<ServerBytes>>(`${this.OdisUrl}/getIpTraffic/$(IP)`)
-    //   .pipe(
-    //     tap((server: ServerBytes[]) => this.logSuccess(`pobrano listę adresów IP`)),
-    //     catchError(this.handleError<Server>(''))
-    //   );
+    // return of(BytesList);
+    return this.http.get<Array<ServerBytes>>(`${this.OdisUrl}/getIpTraffic/$(IP)`)
+      .pipe(
+        tap((server: ServerBytes[]) => this.logSuccess(`pobrano listę adresów IP`)),
+        catchError(this.handleError<ServerBytes>(''))
+      );
   }
 
   setIp(IP: string) {
